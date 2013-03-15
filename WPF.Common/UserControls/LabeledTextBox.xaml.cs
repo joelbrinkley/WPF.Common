33cbca5da8fcdb144a1f6a392a837c0dd76e1bd4
= <jvbrinkley21@gmail.com>
2013-03-15 11:23:34 -0500

    initial load

---
+++ WPF.Common/UserControls/LabeledTextBox.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace WPF.Common.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LabeledTextBox.xaml
+    /// </summary>
+    public partial class LabeledTextBox : UserControl
+    {
+        public LabeledTextBox()
+        {
+            InitializeComponent();
+        }
+
+
+        [Description("The text displayed by the Label")]
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelTextProperty =
+            DependencyProperty.Register("LabelText", typeof(string), typeof(LabeledTextBox), new UIPropertyMetadata(string.Empty));
+
+
+
+        public int LabelMinWidth
+        {
+            get { return (int)GetValue(LabelMinWidthProperty); }
+            set { SetValue(LabelMinWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelMinWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelMinWidthProperty =
+            DependencyProperty.Register("LabelMinWidth", typeof(int), typeof(LabeledTextBox), new UIPropertyMetadata(100));
+
+
+
+
+        [Description("The text displayed by the TextBox")]
+        public string TextBoxText
+        {
+            get { return (string)GetValue(TextBoxTextProperty); }
+            set { SetValue(TextBoxTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextBoxText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextBoxTextProperty =
+            DependencyProperty.Register("TextBoxText", typeof(string), typeof(LabeledTextBox), new UIPropertyMetadata(string.Empty));
+
+
+
+
+        public int TextBoxMinWidth
+        {
+            get { return (int)GetValue(TextBoxMinWidthProperty); }
+            set { SetValue(TextBoxMinWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextBoxMinWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextBoxMinWidthProperty =
+            DependencyProperty.Register("TextBoxMinWidth", typeof(int), typeof(LabeledTextBox), new UIPropertyMetadata(250));
+
+
+        
+
+    }
+}
---
+++ WPF.Common/ViewModel/ViewModelBase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace WPF.Common.ViewModel
+{
+    public class ViewModelBase :  INotifyPropertyChanged
+    {
+        // In ViewModelBase.cs
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        #region [ RequestClose ]
+
+        public event EventHandler RequestClose;
+
+        public void OnRequestClose()
+        {
+            EventHandler handler = this.RequestClose;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+        #endregion
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.VerifyPropertyName(propertyName);
+
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        [Conditional("DEBUG")]
+        [DebuggerStepThrough]
+        public void VerifyPropertyName(string propertyName)
+        {
+            // Verify that the property name matches a real,  
+            // public, instance property on this object.
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                string msg = "Invalid property name: " + propertyName;
+                throw new Exception(msg);
+            }
+        }
+
+        public bool CanPerformRequest
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
